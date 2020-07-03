015a5fd512c1a59d6e955649dff9fba6062158f1
Thiago Macena <thiagosantosmacena@hotmail.com>
2020-07-03 20:39:53 -0300

    adicionando projeto agencia carros

---
+++ sites-navegacao/projeto_05/js/function.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+    $(".mobile").click(function(){
+        $("nav.mobile ul").slideToggle("slow"); 
+        //$(".mobile").css("background-image","url('../imagens/fechar.png')");
+
+        
+    });
+
+
+
+})
